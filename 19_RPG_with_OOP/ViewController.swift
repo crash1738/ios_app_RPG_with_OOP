e122a3ad45c2ef12377c5b8662ae81a8a322ccf6
蔡昆翰 <crash1738@hotmail.com>
2016-01-31 11:40:41 +0800

    Initial Commit

---
+++ 19_RPG_with_OOP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  19_RPG_with_OOP
+//
+//  Created by 昆翰 蔡 on 2016/1/31.
+//  Copyright © 2016年 昆翰 蔡. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
